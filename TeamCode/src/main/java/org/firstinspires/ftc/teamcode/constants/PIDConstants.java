75b2346e794a0336b5e781a2a9c73367adad767a
Kai Cortadellas <kai.cortadellas@pinecrest.edu>
2025-12-14 13:08:47 -0500

    PID Tuned

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/PIDConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/PIDConstants.java
@@ -5,28 +5,23 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class PIDConstants {
     // Constants dealing with moving forward and backward
-    public static double DRIVE_KP = 0.36;
+    public static double DRIVE_DEADBAND = 0.25;
+    public static double DRIVE_KD = 0.025;
     public static double DRIVE_KI = 0;
-    public static double DRIVE_KD = 0;
-    public static double DRIVE_TOLERANCE = 0.5;
-    public static double DRIVE_DEADBAND = 0.25; // must be less than tolerance
+    public static double DRIVE_KP = 0.2;
     public static double DRIVE_MAX_AUTO = 0.8;
-
-
-    // Constants dealing with moving side to side
-    public static double STRAFE_KP = 0.5;//0.5
+    public static double DRIVE_TOLERANCE = 0.5;
+    public static double STRAFE_DEADBAND = 0.25;
+    public static double STRAFE_KD = 0.025;
     public static double STRAFE_KI = 0;
-    public static double STRAFE_KD = 0;
-    public static double STRAFE_TOLERANCE = 0.5;
-    public static double STRAFE_DEADBAND = 0.25; //must be less than tolerance
+    public static double STRAFE_KP = 0.2;
     public static double STRAFE_MAX_AUTO = 0.8;
-
-
-    // Constants dealing with turning
-    public static double YAW_KP = 0.1;//0.5
+    public static double STRAFE_TOLERANCE = 0.5;
+    public static double YAW_DEADBAND = 0.25;
+    public static double YAW_KD = 0.0035;
     public static double YAW_KI = 0;
-    public static double YAW_KD = 0;
-    public static double YAW_TOLERANCE = 3;//1
-    public static double YAW_DEADBAND = .25;
+    public static double YAW_KP = 0.045;
     public static double YAW_MAX_AUTO = 0.8;
+    public static double YAW_TOLERANCE = 3;
+
 }
